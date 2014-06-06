46045ab66bd1dc5a9fe9bb781c619370ff4cbce8
Song Gao <song@gao.io>
2014-06-06 14:44:48 -0500

    add author/license/repo info

--- package.lua
+++ package.lua
@@ -1,7 +1,28 @@
 return {
   name = "tape",
-  version = "0.1",
+  version = "0.1.0",
   description = "TAP producer for luvit",
+  repository = {
+    url = "https://github.com/virgo-agent-toolkit/luvit-tape.git",
+  },
+  author = {
+    name = "Song Gao",
+    email = "song@gao.io",
+    url = "https://song.gao.io",
+  },
+  contributors = {
+    {
+      name = "Robert Chiniquy",
+      email = "robert.chiniquy@rackspace.com",
+      url = "https://robert-chiniquy.github.io/",
+    },
+    {
+      name = "Ryan Phillips",
+      email = "ryan.phillips@rackspace.com",
+      url = "http://trolocsis.com/",
+    },
+  },
+  licenses = {"Apache-2.0"},
   dependencies = {
     "stream",
   },
