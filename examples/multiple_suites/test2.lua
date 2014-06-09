00feba503f20444822854bbfa91c082bff7ad9fa
Song Gao <song@gao.io>
2014-06-09 13:23:38 -0500

    use multiple test files for multiple test suites

---
+++ examples/multiple_suites/test2.lua
@@ -0,0 +1,7 @@
+local test = require('../..')("test_2")
+
+test("My super lame test", nil, function(t)
+  t:is_number(42, "42 should be a number")
+  t:is_number({}, "is {} a number?")
+  t.finish()
+end)
