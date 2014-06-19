921cc42332b03816aafb50934b26687b9f8260ea
Song Gao <song@gao.io>
2014-06-18 21:58:28 -0500

    use resume() instead of a dedicated Sink

--- init.lua
+++ init.lua
@@ -14,22 +14,11 @@ process:on('exit', function(exit_code)
   end
 end)
 
-local Sink = require('stream').Writable:extend()
-
-function Sink:initialize()
-  require('stream').Writable.initialize(Sink, {objectMode = true})
-end
-
-function Sink:_write(data, encoding, cb)
-  cb()
-end
-
 -- Test suites are triggered in the next tick
 process.nextTick(function()
   local runner = Runner:new()
-  suites:pipe(runner):pipe(Sink:new())
-  -- suites:pipe(runner)
-  -- runner:resume() -- trigger flow mode so that no consumer is needed
+  suites:pipe(runner)
+  runner:resume() -- trigger flow mode so that no consumer is needed
 end)
 
 -- all test functions should be passed in within the first tick
